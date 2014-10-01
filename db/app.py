f571f26f3cf0d207aafabb8e1f00c5a99c6236ee
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2014-10-01 19:50:34 -0400

    connects to the database, returns error if clusterName doesn't exist

--- db/app.py
+++ db/app.py
@@ -3,15 +3,24 @@ from bson.objectid import ObjectId
 import time
 import hashlib
 
-client = MongoClient('mongodb://localhost:27017/')
-db = client['BigData']
+#client = MongoClient('mongodb://localhost:27017/') # for use in localhost
+#db = client['BigData'] # for use in localhost
+client = MongoClient('mongodb://146.148.59.202:27017/')
+db = client['big_data']
 
 def getCluster(clusterName):
     cluster = db.clusters.find({ "clusterName": clusterName })
 
+    if cluster.count() <= 0:
+        return {
+            "error": "Error: No " + clusterName + " cluster found."
+        }
+
     articles = []
     for articleId in cluster[0][u'articles']:
         article = db.articles.find_one({ "_id": articleId })
+        # There must be a more effective way to get all the articles
+        # without making the cursor go over the db multiple times...
         articles.append(article)
 
     return {
