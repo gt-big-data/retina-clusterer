b92558c21cd32273b8055aab072cc58b475a80d8
Joshua Koh <Joshua@kohfamily.com>
2015-02-04 18:56:38 -0500

    Added getTrainingSet(limit = 0)

--- db/testSpace.py
+++ db/testSpace.py
@@ -7,10 +7,13 @@ from bson.json_util import dumps
 import time
 import hashlib
 import md5
-from app import getClusterArticleCount, client, db
+from app import getClusterArticleCount, getArticleClusterList, client, db
 from datetime import datetime, timedelta
+from collections import namedtuple
 
-def updatedGetCluster(clusterName, limit = 0):
+Article = namedtuple('Article', ['title', 'text', 'categories', 'id'])
+
+def getLatestCluster(clusterName, limit = 50):
     cluster = db.clusters.find_one({ "clusterName": clusterName })
     if limit == 0:
         limit = getClusterArticleCount(clusterName);
@@ -19,12 +22,28 @@ def updatedGetCluster(clusterName, limit = 0):
             "error": "Error: No " + clusterName + " cluster found."
         }
     articles = db.articles.find ( { "$query": { "_id":  { "$in": cluster["articles"] } }, "$orderby": { 'recent_pub_date' : -1 } } ).limit(limit)
-    # "$orderby": { 'recent_pub_date' : -1 },
-    count = 0
-    for a in articles:
-        count += 1
-        print a['title']
-    return count;
-    # return len(dumps(articles));
 
-print updatedGetCluster("Politics", 41)
+    clean_articles = [];
+    for article in articles:
+        clean_articles.append(Article(article['title'], article['text'], article['categories'], article['_id']))
+
+    return clean_articles
+
+#print getLatestCluster("Sports");
+
+def getTrainingSet(limit = 0):
+    clusterList = getArticleClusterList()
+    trainingSet = []
+    
+    for cluster in clusterList:
+        trainingSet.append(getLatestCluster(cluster, limit))
+
+    return trainingSet
+
+print getTrainingSet(1);
+
+# // See articleLoader file
+# // see nametuble in file
+# // TODO getTrainingSet() -> for loops every category with getLatestCluster(...). returns the articles
+# // for cluster in clusterNames:
+# // article[title], article[text], cluster's name, article[id]
