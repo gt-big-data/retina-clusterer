c40952a936acd656dc6cd798fce575c58acc354b
Fan Chen <cffls@hotmail.com>
2015-03-04 20:02:01 -0500

    get_similar_articles added

--- get_similar_articles.py
+++ get_similar_articles.py
@@ -11,11 +11,13 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.svm import SVC
 
 
-def getSimilarArticles(article, numOfDays = 10, numOfNeighbors):
-	articles = app.getTrainingSet(50, 0)
+def getSimilarArticles(article, numOfDays, numOfNeighbors):
+	articles = app.getTrainingSet(40, 1)
 	neigh = KNeighborsClassifier()
+	count_vect = CountVectorizer(stop_words='english')
+	tfidf_trans = TfidfTransformer()
 	trainingText = [x.text for x in articles]
-	rainingLabels = [x.categories for x in trainingArticles]
+	rainingLabels = [x.categories for x in articles]
 
 	trainingCounts = count_vect.fit_transform(trainingText)
 	trainingTfidf = tfidf_trans.fit_transform(trainingCounts)
@@ -24,7 +26,17 @@ def getSimilarArticles(article, numOfDays = 10, numOfNeighbors):
 	targetTfidf = tfidf_trans.transform(targetCounts)
 
 	neigh.fit(trainingTfidf,rainingLabels)
-	neigh.kneighbors(n_neighbors=numOfNeighbors, return_distance=False)
+	similar_articles_tfidf = neigh.kneighbors(targetTfidf, numOfNeighbors, False)
+	similar_articles = []
+	for index in similar_articles_tfidf[0]:
+		similar_articles.append(articles[index].title)
+	return similar_articles
 
 
-trainingArticles = app.getTrainingSet(50, 0)
+trainingArticles = app.getTrainingSet(1, 0)
+target = trainingArticles[4]
+print target.title
+
+similar_articles = getSimilarArticles(target, 10, 4)
+for article in similar_articles:
+	print article
