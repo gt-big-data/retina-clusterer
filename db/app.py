47bb50bdba9635dcaf730d026c6e21a15fbfb151
Joshua Koh <Joshua@kohfamily.com>
2015-03-04 19:34:30 -0500

    Updated getArticlesInLastNDays(), removed small typo

--- db/app.py
+++ db/app.py
@@ -27,8 +27,8 @@ def getArticlesByTimeStamp(timeStamp, limit=1000):
         returnObject.append(Article(article['title'], article['text'], cat, article['recent_pub_date'], article['_id'])) # this is old categories, be careful
     return returnObject
 
-def getArticlesInLastNDays(n = 10, limit=1000):
-    timeObj = datetime.now() - timedelta(days=15);
+def getArticlesInLastNDays(n=10, limit=1000):
+    timeObj = datetime.now() - timedelta(days=n);
     version = getCrawlerVersion()
     articles = db.articles.find({'$and': [{"v": version}, {"text": {'$ne': ''}}, {"title": {'$ne': ''}}, {"recent_download_date": {"$gte":  timeObj}}]}).limit(limit);
     returnObject = [];
