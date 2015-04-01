824679c4338fab19276275bdfeacf5a5e03b8f59
github.gatech.edu <ngoel32@gatech.edu>
2015-04-01 19:52:08 -0400

    basic name entity working

--- name_entity_test.py
+++ name_entity_test.py
@@ -1,34 +1,23 @@
 import nltk
 
-with open('Test_Articles/Fairly bad pitcher traps triumph in the end.txt', 'r') as f:
+#with open('Test_Articles/Fairly bad pitcher traps triumph in the end.txt', 'r') as f:
+with open('Test_Articles/American held by ISIS.txt', 'r') as f:
     sample = f.read()
-     
-sentences = nltk.sent_tokenize(sample.encode('utf-8'))
-tokenized_sentences = [nltk.word_tokenize(sentence) for sentence in sentences]
-tagged_sentences = [nltk.pos_tag(sentence) for sentence in tokenized_sentences]
-chunked_sentences = nltk.ne_chunk_sents(tagged_sentences, binary=True)
 
-def extract_entity_names(t):
-    entity_names = []
-    
-    if hasattr(t, 'node') and t.node:
-        if t.node == 'NE':
-            entity_names.append(' '.join([child[0] for child in t]))
-        else:
-            for child in t:
-                entity_names.extend(extract_entity_names(child))
-                
-    return entity_names
+sentences = nltk.sent_tokenize(sample.encode('utf-8')) #seperate into sentences
+tokenized_sentences = [nltk.word_tokenize(sentence) for sentence in sentences] #seperate into words
+tagged_sentences = [nltk.pos_tag(sentence) for sentence in tokenized_sentences] #add part of speech to each word
+chunked_sentences = nltk.ne_chunk_sents(tagged_sentences, binary=True) #turn list into tree
 
-entity_names = []
-for tree in chunked_sentences:
-    # Print results per sentence
-    # print extract_entity_names(tree)
-    
-    entity_names.extend(extract_entity_names(tree))
-
-# Print all entity names
-#print entity_names
-
-# Print unique entity names
-print set(entity_names)
+nameEntity = []
+for sentence in chunked_sentences:
+    for part in sentence: #part is tuple or tree
+        if type(part) is nltk.Tree: #name entities are only in trees
+            listNNP = part.leaves()
+            entity = ""
+            for tupleNNP in listNNP: #turn tree into entity
+                entity += " "
+                entity += tupleNNP[0]
+            entity = entity[1:]
+            nameEntity.append(entity) #add entity to list
+print nameEntity
