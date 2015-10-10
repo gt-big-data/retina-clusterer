72c423276390c9f6ff1b3d4b4238a746ad91c389
Patrick Violette <violette.patrick718@gmail.com>
2015-10-09 23:01:30 -0400

    Added keywords for topics

--- 4-topicMatch.py
+++ 4-topicMatch.py
@@ -23,7 +23,7 @@ def generateGraphForDay(endTime):
 	
 	maxTopic = art[0]['topic']
 	print maxTopic
-	nodesClean = []
+	updatedTopics = []
 	edgesClean = []
 	g = Graph()
 	g.add_vertices(len(articles))
@@ -44,7 +44,6 @@ def generateGraphForDay(endTime):
 	oldTopics = []
 	for i, membership in zip(range(0,len(articles)-1), memberships):
 		oldTopics.append(articles[i].topic)
-		nodesClean.append({"id": articles[i].guid, "name": articles[i].title.encode('utf-8').replace('"', ''), "group": str(membership), "keywords": articles[i].keywords[:5], "img": '', 'source': articles[i].source, 'url': articles[i].url})
 
 	for bigComm in bigCommList:
 		oldTopicList = []
@@ -56,16 +55,48 @@ def generateGraphForDay(endTime):
 
 		newTopic = 0
 		nonZeroTopic = [v for v in oldTopicList if v != 0]
+		updatedTopics.extend(nonZeroTopic)
 		if len(nonZeroTopic) == 0:
 			maxTopic += 1
 			newTopic = maxTopic
 		else:
 			counter = Counter(nonZeroTopic)
 			newTopic = counter.most_common(1)[0][0]
+		updatedTopics.append(newTopic)
 		db.qdoc.update({"_id": {'$in': idList}}, {"$set": {"topic": newTopic}}, multi=True)
+		return updatedTopics
 
-#daySpan = 40
-#startFrom = time.time()-daySpan*86400
-#for i in range(0,daySpan):
-#	generateGraphForDay(startFrom+i*86400)
-generateGraphForDay(time.time())
+def updateTopicKeywords(topicIds):
+	keywordsAndTopics = db.qdoc.aggregate([
+		{'$match': {'topic': {'$in': topicIds}}},
+		{'$project': {'topic': True, 'keywords': True}},
+		{'$unwind': '$keywords'},
+		{'$group': {
+			'_id': {'topic': '$topic', 'keyword': '$keywords'},
+			'count': {'$sum': 1}}},
+		{'$group': {
+			'_id': '$_id.topic',
+			'keywords': {
+				'$push' : {'keyword': '$_id.keyword', 'count': '$count'}},
+		}},
+		{'$unwind': '$keywords'},
+		{'$sort': {'_id': -1, 'keywords.count': -1}},
+		{'$group': {
+			'_id': '$_id',
+			'keywords': {'$push' : {'keyword': '$keywords.keyword', 'count': '$keywords.count'}},
+			'totalKeywords': {'$sum': '$keywords.count'}}},
+	], allowDiskUse=True)
+
+	topicUpdate = db.topic.initialize_unordered_bulk_op()
+	for topic in keywordsAndTopics:
+		topicUpdate.find({'_id': topic['_id']}).upsert().update({
+			'$set': {
+				'keywords': topic['keywords'][:10],
+				'totalKeywords': topic['totalKeywords'],
+			},
+		})
+	topicUpdate.execute()
+
+if __name__ == '__main__':
+	updatedTopics = generateGraphForDay(time.time())
+	updateTopicKeywords(updatedTopics)
